c07f03255702b4c975cd8d433f1292eb1752ae99
bahauddin <baha200477@gmail.com>
2025-02-15 01:04:42 +0500

    updates for host

--- main.py
+++ main.py
@@ -111,6 +111,11 @@ if __name__ == "__main__":
     if not SHOW_UI:
         print("Running in headless mode")
         options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--user-data-dir=/tmp/chrome-data')
+    options.add_argument('--remote-debugging-port=9222')
 
     driver = webdriver.Chrome(options=options)
     try:
